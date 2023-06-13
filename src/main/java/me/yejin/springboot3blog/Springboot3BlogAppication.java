d12f6f643b8881df260eaeaf2c0b17ee8c4d585e
yjseo <yjseo@iochord.com>
2023-06-13 14:44:19 +0900

    chore: 기본 환경 설정

---
+++ src/main/java/me/yejin/springboot3blog/Springboot3BlogAppication.java
@@ -0,0 +1,16 @@
+package me.yejin.springboot3blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * author : yjseo
+ * <p>
+ * date : 2023-06-13
+ */
+@SpringBootApplication
+public class Springboot3BlogAppication {
+  public static void main(String[] args) {
+    SpringApplication.run(Springboot3BlogAppication.class, args);
+  }
+}
