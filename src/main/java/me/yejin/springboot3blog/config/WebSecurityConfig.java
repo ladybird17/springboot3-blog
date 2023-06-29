a0ba9a23bbfad53332664cd604cd3aeaa144b041
yjseo <yjseo@iochord.com>
2023-06-29 17:55:07 +0900

    feat: Spring boot 3.1.0으로 업데이트 이후 deprecated된 security 기능 수정

--- src/main/java/me/yejin/springboot3blog/config/WebSecurityConfig.java
+++ src/main/java/me/yejin/springboot3blog/config/WebSecurityConfig.java
@@ -35,31 +35,26 @@ public class WebSecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     //특정 HTTP 요청에 대한 웹 기반 보안 구성
-    return http
-        .authorizeHttpRequests()//인증, 인가 설정
-        .requestMatchers("/login", "/signup", "/user").permitAll()
-        .anyRequest().authenticated()
-        .and()
-        .formLogin()//폼 기반 로그인 설정
-        .loginPage("/login")
-        .defaultSuccessUrl("/articles")
-        .and()
-        .logout()//로그아웃 설정
-        .logoutSuccessUrl("/login")
-        .invalidateHttpSession(true)
-        .and()
-        .csrf().disable()//csrf 비활성화
+    return http.authorizeHttpRequests(authorize -> authorize
+          .requestMatchers("/login", "/signup", "/user").permitAll()
+          .anyRequest().authenticated()
+        )//인증, 인가 설정
+        .formLogin(login -> login.loginPage("/login").permitAll().defaultSuccessUrl("/articles")
+        )//폼 기반 로그인 설정
+        .logout(logout -> logout.logoutSuccessUrl("/login").invalidateHttpSession(true)
+        )//로그아웃 설정
+        .csrf(csrf -> csrf.disable()
+        )//csrf 비활성화
         .build();
   }
 
   @Bean
   public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailService userDetailService) throws Exception {
     //인증 관리자 관련 설정
-    return http.getSharedObject(AuthenticationManagerBuilder.class)
-        .userDetailsService(userService)//사용자 정보 서비스 설정
-        .passwordEncoder(bCryptPasswordEncoder)
-        .and()
-        .build();
+    AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        builder.userDetailsService(userService)//사용자 정보 서비스 설정
+        .passwordEncoder(bCryptPasswordEncoder);
+    return builder.build();
   }
 
   @Bean
